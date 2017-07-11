f013e4bba9e81735d383adb70531cc0b0ce1b864
jaymomnang <juliusmomnang@gmail.com>
2017-07-11 11:07:21 +0100

    api testing

--- server.js
+++ server.js
@@ -1,37 +1,23 @@
-
 var express = require('express'),
   app = express(),
   port = process.env.PORT || 3100,
   mongoose = require('mongoose'),
-  invoice = require('./api/models/invoiceModel'),
+  Task = require('./api/models/todoListModel'),
   bodyParser = require('body-parser');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/moringadb', { useMongoClient: true }, function(err, db){
+mongoose.connect('mongodb://localhost:27017/Tododb');
 
-  console.log("Successfully connected to database.");
-  app.use(bodyParser.urlencoded({ extended: true }));
-  app.use(bodyParser.json());
 
-  var routes = require('./api/routes/invoiceRoutes');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
-  var router = express.Router();
-  router.get('/', function(req, res) {
-      res.send({ message: 'hooray! welcome to our api!' });
-      res.json({ message: 'hooray! welcome to our api!' });
-  });
 
-  app.use('/api', router);
-  routes(app);
+var routes = require('./api/routes/todoListRoutes');
+routes(app);
 
-  app.listen(port);
-  console.log('Apene Accounting API server started on: ' + port);
 
-});
+app.listen(port);
 
-//MongoClient.connect('mongodb://localhost:27017/video', function(err, db) {
 
-//    assert.equal(null, err);
-//    console.log("Successfully connected to MongoDB.");
-
-//});
+console.log('todo list RESTful API server started on: ' + port);
